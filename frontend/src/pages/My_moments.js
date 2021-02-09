92d99d01a65bdf9d25e355e4ed03652b0bcabe9b
Sabrina Akbar <sabrinaakbar141@gmail.com>
2021-02-09 23:08:31 +0530

    fixed the image in my moments screen (#73)

--- frontend/src/pages/My_moments.js
+++ frontend/src/pages/My_moments.js
@@ -2,25 +2,53 @@ import React, { useState } from "react";
 import ImgGrid from "../component/photo_coms/folders/imgGrid";
 import Upload from "../component/photo_coms/folders/Upload";
 import Modal from "../component/photo_coms/folders/modal";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import CssBaseLine from "@material-ui/core/CssBaseline";
 
 function My_moments() {
   const [selectedImg, setselectedImg] = useState();
 
   return (
-    <div className="App">
-      <div className="title">
-        <h2>My Moments</h2>
+    <div>
+      <div className="welcome">
+        <CssBaseLine />
+        <Box mt={9}>
+          <Typography variant="h4">Photos</Typography>
+          <Box mt={5}>
+            <Typography variant="h5">
+              Let's go back to this moment and live it in forever
+            </Typography>
+            <Typography variant="h5">Take me away to better days</Typography>
+          </Box>
+        </Box>
       </div>
-      <div className="img-grid">
-        <div className="img-wrap">
-          <Upload />
-          <p className="upload">Upload</p>
+      <div className="Photocontent">
+        <Typography
+          variant="h4"
+          style={{ color: "white", paddingLeft: "4rem" }}
+        >
+          My Moments
+        </Typography>
+        <div className="PhotoApp">
+          <div className="img-grid">
+            <div className="img-wrap">
+              <Upload />
+              <p className="upload">Upload</p>
+            </div>
+          </div>
+
+          {selectedImg && (
+            <>
+              <ImgGrid setselectedImg={setselectedImg} />
+              <Modal
+                selectedImg={selectedImg}
+                setselectedImg={setselectedImg}
+              />
+            </>
+          )}
         </div>
       </div>
-      <ImgGrid setselectedImg={setselectedImg} props={`users/0/images`} />
-      {selectedImg && (
-        <Modal selectedImg={selectedImg} setselectedImg={setselectedImg} />
-      )}
     </div>
   );
 }
