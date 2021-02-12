3686d4c4743a34fd4f59d12a6695a3fcc4486437
JakeKielmeyer <66395612+JakeKielmeyer@users.noreply.github.com>
2021-02-12 14:04:19 -0500

    Fix my moments (#88)
    * added token to the userstorage
    
    * added image grid and removed cors in backend
    
    * removed temp user in upload and delete functions
    
    * removed temp user var in upload function
    
    * Fixed the routing
    
    * added image grid and removed cors in backend
    
    * removed temp user in upload and delete functions
    
    * removed temp user var in upload function
    
    * added delete functionality to modal
    
    Co-authored-by: Sabrina Akbar <sabrinaakbar141@gmail.com>

--- frontend/src/component/photo_coms/folders/modal1.js
+++ frontend/src/component/photo_coms/folders/modal1.js
@@ -1,39 +1,49 @@
-import React from 'react'
-import Carousel from 'react-material-ui-carousel'
+import React from "react";
+import Carousel from "react-material-ui-carousel";
 import useFirestore from "../../../hooks/useFirestore";
+import axios from "axios";
 
+function Modal1({ selectedImg, setselectedImg, props }) {
+  const { docs, imgarr } = useFirestore(props);
 
-function Modal1({selectedImg, setselectedImg,props}) {
-const { docs, imgarr } = useFirestore (props);
-
- const handleClick = (e)=> {
+  const handleClose = (e) => {
     setselectedImg(null);
- }
- 
-    return (
-        <div className="backdrop"  >
-             
-            <button className="close" style={{width:"100%"}}
-            onClick={handleClick}
-            >Close</button>
-           
-            
-            <Carousel
-             index={imgarr.indexOf(selectedImg.id)}
-              autoPlay={false}
-              fullHeightHover={true}
-              animation={'fade'}
-              timeout={12}
-              indicatorContainerProps={{style:{width:"0px", height:"0px", 
-             marginTop:'60%'}}}
-              navButtonsAlwaysVisible={true}
-            >   
-            {selectedImg && docs.map((doc)=> <img src={doc.url} alt={doc.name}/>)}
-            </Carousel> 
-            
-            
-        </div>
-    )
+  };
+  const handleDelete = (e) => {
+    const config = {
+      headers: {
+        Authorization: localStorage.FBIdToken,
+      },
+      data: {
+        name: selectedImg.name,
+      },
+    };
+    axios.delete("/image", config).then(() => setselectedImg(null));
+  };
+
+  return (
+    <div className="backdrop">
+      <button className="close" style={{ width: "50%" }} onClick={handleClose}>
+        Close
+      </button>
+      <button className="close" style={{ width: "50%" }} onClick={handleDelete}>
+        Delete Image
+      </button>
+      <Carousel
+        index={imgarr.indexOf(selectedImg.id)}
+        autoPlay={false}
+        fullHeightHover={true}
+        animation={"fade"}
+        timeout={12}
+        indicatorContainerProps={{
+          style: { width: "0px", height: "0px", marginTop: "60%" },
+        }}
+        navButtonsAlwaysVisible={true}
+      >
+        {selectedImg && docs.map((doc) => <img src={doc.url} alt={doc.name} />)}
+      </Carousel>
+    </div>
+  );
 }
 
 export default Modal1;
