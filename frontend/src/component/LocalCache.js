4a566a6158a4d26da6281d5e5da4f9897ad0a5b4
zzoghi <zzoghi@rockets.utoledo.edu>
2021-03-13 15:52:27 -0500

    Video cache and Music cache is done

---
+++ frontend/src/component/LocalCache.js
@@ -0,0 +1,27 @@
+let db = {
+  musicFolders: [
+    "70s Time Machine: Top Hits of the 1970s",
+    "Boomer Heyday: Top Hits of the 1960s",
+    "Pop-Socks! Top Hits of the 1950s",
+  ],
+  musics: [
+    {
+      musicAdd:
+        "musics%2FPop-Socks! Top Hits of the 1950s%2FBuddyHollyEveryday.mp3",
+      musicToken: "d1852f43-a08a-4f33-949c-28256d99f3c2",
+    },
+  ],
+  musicInfo: [
+    {
+      musicAlbum: "Parallel Lines",
+      musicTrack: "Heart of Glass",
+      musicArtist: "Blondie",
+    },
+  ],
+  musicOnClick: [
+    {
+      click: 0,
+      musicName: "BlondieHeartOfGlass.mp3",
+    },
+  ],
+};
