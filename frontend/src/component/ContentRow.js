7962ad90b74692d8c25d599a6abbc9e8543e914c
bhzoghi <bh.zoghi@hotmail.com>
2021-06-08 19:37:34 -0400

    directing from dashboard to movie page is done

--- frontend/src/component/ContentRow.js
+++ frontend/src/component/ContentRow.js
@@ -2,6 +2,7 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
+import { useHistory } from "react-router";
 
 const styles = {
   row: {
@@ -18,7 +19,7 @@ const styles = {
       width: "2px",
     },
   },
-  
+
   gridListTile: {
     width: "18rem !important",
     height: "18rem !important",
@@ -33,66 +34,70 @@ const styles = {
     objectFit: "cover",
     width: "17.8rem",
     height: "17.6rem",
-    boxSizing:"border-box",
+    boxSizing: "border-box",
     border: "2px solid rgba(245, 245, 245, 0.76)",
     borderRadius: "2%",
     boxShadow: "0 4px 8px 0",
-    
   },
 
-  title:{
+  title: {
     overflow: "hidden",
     fontSize: "1.3rem",
     lineHeight: "24px",
     whiteSpace: "nowrap",
     textOverflow: "ellipsis",
-    },
+  },
 
-    titleBar:{
+  titleBar: {
     left: "2px",
     right: "2px",
-    bottom:"2px",
+    bottom: "2px",
     height: "4em",
     display: "flex",
     position: "absolute",
     background: "rgba(0, 0, 0, 0.65)",
     alignItems: "center",
     fontFamily: "KOW, sans-serif",
-    border:"2%",
+    border: "2%",
     transition: "height .7s",
     "&:hover": {
       backgroundColor: "rgba(0, 0, 0, .9)",
-      height:"98.5%",
-    
+      height: "98.5%",
     },
-},
-
-  }
-
+  },
+};
 
 function ContentRow(props) {
+  const history = useHistory();
   const { classes } = props;
   return (
     <div className={classes.row}>
       <GridList className={classes.gridList} cols={2.5}>
         {props.images.map((image) => (
           <GridListTile key={image.title} className={classes.gridListTile}>
-            <a href={image.href}>
-              <img
-                src={image.url}
-                alt={image.caption}
-                className={classes.image}
-              />
-              
-              <GridListTileBar
-                title={image.caption}
-                classes={{
-                  root: classes.titleBar,
-                  title: classes.title,
-                }}
-                style={{textAlign:"center"}}
-              />
-            </a>
+            {/* <a href={image.href}> */}
+            {/* <a onClick={history.push(`/${image.href}`)}> */}
+            <img
+              src={image.url}
+              alt={image.caption}
+              className={classes.image}
+              onClick={() =>
+                history.push({
+                  pathname: `/${image.href}`,
+                  genreDash: `${image.caption}`,
+                })
+              }
+            />
+
+            <GridListTileBar
+              title={image.caption}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              style={{ textAlign: "center" }}
+            />
+            {/* </a> */}
           </GridListTile>
         ))}
       </GridList>
